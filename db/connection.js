79080dccfb15310e72a083f95268d95bad134f73
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-20 14:34:32 -0700

    created all initial files

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+// CONNECT TO DATABASE
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL user,
+        user: 'root',
+        //Your MYSQL pass,
+        password: 'Homebrewed2003!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
