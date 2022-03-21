b1747c9fe139dec52926f5fe39abeeeb989c57c5
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-20 18:10:18 -0700

    created Seeds and Schema tables

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS job;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+/* replaced role with job because role was highlighted and I didnt trust it */
+CREATE TABLE job (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    job_id INT NULL,
+    manager_id INT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (id, name)
+VALUES
+    (1, 'Paint'),
+    (2, 'Photography'),
+    (3, 'Sketch');
+
+INSERT INTO job (title, salary, department_id)
+VALUES
+    ('Painter', 80000, 1),
+    ('Photographer', 60000, 2),
+    ('Photo Printer', 40000, 2),
+    ('Sketch artist', 60000, 3),
+    ('Sketch Instructor', 60000, 3);
+
+INSERT INTO employee (first_name, last_name, job_id, manager_id)
+VALUES
+    ('Mahogany', 'Wood', 1, 2),
+    ('Free', 'Style', 1, NULL),
+    ('Mike', "Pilgrim", 2, NULL),
+    ('Expo', 'Mark', 3, 3),
+    ('Strath', 'More', 4, 5)
+    ('Jeremy', 'Lead', 5, NULL);
