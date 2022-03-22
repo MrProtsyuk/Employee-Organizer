15264a4ebee9ca445dcb9dcd50ff69f3fcd43ed7
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-21 17:52:54 -0700

    created addRoll and addEmployee functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
+const { fetchAsyncQuestionProperty } = require('inquirer/lib/utils/utils');
 require('console.table');
 
 db.connect((err) => {
@@ -113,14 +114,71 @@ const addDepartment = () => {
 
 // Add a roll within a department
 const addRoll = () => {
+    inquirer
+    .prompt ([{
+        type: 'text',
+        name: 'title',
+        message: 'What is the role title?',
+    },
+    {
+        type : 'text',
+        name: 'salary',
+        message: 'What is the salary for this role?',
+    },
+    {
+        type: 'text',
+        name: 'department_id',
+        message: 'What is the department ID?'
+    }]).then((answer) => {
     const sql = `INSERT INTO job (title, salary, department_id)
     VALUES (?,?,?)`;
+    const params = [answer.title, answer.salary, answer.department_id];
+        db.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.table(res)
+            console.log('Created a New Roll! Press View Rolls to see!')
+
+            //Return user to prompt
+            return promptUser();
+        })
+    });
 }
 
 // Add and employee, give them a role, within a department
 const addEmployee = () => {
+    inquirer
+    .prompt ([{
+        type: 'text',
+        name: 'first_name',
+        message: 'What is their first name?',
+    },
+    {
+        type : 'text',
+        name: 'last_name',
+        message: 'What is their last name?',
+    },
+    {
+        type: 'text',
+        name: 'job_id',
+        message: 'What is their role ID?'
+    },
+    {
+        type: 'text',
+        name: 'manager_id',
+        message: 'What is this employees Manager Id?',
+    }]).then((answer) => {
     const sql = `INSERT INTO employee (first_name, last_name, job_id, manager_id)
     VALUES (?,?,?,?)`;
+    const params = [answer.first_name, answer.last_name, answer.job_id, answer.manager_id];
+    db.query(sql, params, (err, res) => {
+        if (err) throw err;
+        console.table(res)
+        console.log('Created a New Employee! Press View Employees to see!')
+
+        //Return user to prompt
+        return promptUser();
+        })
+    });
 }
 
 // Updating an employee role
